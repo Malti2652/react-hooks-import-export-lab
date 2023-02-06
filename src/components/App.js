2be67ae2760b636541db0980f10282944ee856c9
Eddington Rowellis <eddingtonibi@gmail.com>
2023-02-06 16:17:27 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import About from "./About";
+import NavBar from "./NavBar";
+import Home from "./Home";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home username="Liza" city="New York" />
+      <About image="https://i.imgur.com/mV8PQxj.gif" />
     </div>
   );
 }
